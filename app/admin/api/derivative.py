e7da372a9c11698728cbf756b6c3c97d1bf53875
DonerKebab <mrphuongdee@gmail.com>
2018-02-08 10:46:20 +0700

    add default date to supplyDemand

--- app/admin/api/derivative.py
+++ app/admin/api/derivative.py
@@ -118,10 +118,10 @@ def supplyDemand():
 
     """
     args = {k: v for k, v in app.trade_parser.parse_args().iteritems() if v is not None}
-
+    if args.get('fromDate') is None and args.get('toDate') is None :
+        args['tradingDate'] = datetime.datetime.today().strftime('%Y-%m-%d')
     # get filters
     filters = app.trade_parser.get_trade_filters(args)
-
     sort = [
             {"symbol": {"order": "ASC"}},
             {"tradingDate": {"order": "DESC"}},
